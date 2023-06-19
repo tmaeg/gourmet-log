21dac939cbcae67645b778cb6ea5044706465b86
tmaeg <61919505+tmaeg@users.noreply.github.com>
2023-06-19 13:22:50 +0000

    Localized

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import './myTranslate.js';
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
